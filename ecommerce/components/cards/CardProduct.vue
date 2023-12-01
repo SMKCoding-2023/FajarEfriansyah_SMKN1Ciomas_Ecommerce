36d1ddb36e2170aaa69a1a62f6b3234a0a0e0e90
Fjr113 <fajarefriansyah13@gmail.com>
2023-12-01 12:31:54 +0700

    first commit

--- ecommerce/components/cards/CardProduct.vue
+++ ecommerce/components/cards/CardProduct.vue
@@ -1,34 +1,84 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 const props = defineProps({
-product: {
-type: Object,
-default: {},
-}
+  product: {
+    type: Object,
+    default: {},
+  },
 });
 
 const { baseStorageUrl } = useAppConfig();
+
+const isDeleting = ref(false);
+const router = useRouter();
+
+const deleteProduct = async () => {
+  // You can implement the logic to delete the product here
+  // For example, you can call an API to delete the product
+  try {
+    // Assuming you have an API endpoint for deleting a product
+    await api.delete(`/products/${props.product.id}`);
+    // Optionally, you can emit an event to inform the parent component that the product is deleted
+    // emit('productDeleted', props.product.id);
+    // Redirect to a different route or perform other actions as needed
+    router.push('/products');
+  } catch (error) {
+    console.error('Error deleting product', error);
+  }
+};
 </script>
+
 <template>
-    <section class="bg-white shadow-xl rounded-xl overflow-hidden">
-    <div :class="`w-full h-[200px] p-5 bg-gray-300`">
-    <img :src="baseStorageUrl + props.product.image" class="w-full h-full
-    object-contain"/>
+  <section class="bg-white shadow-xl rounded-xl overflow-hidden">
+    <div :class="`w-full h-[200px] p-5 bg-blue-300`">
+      <img
+        :src="baseStorageUrl + props.product.image"
+        class="w-full h-full object-contain"
+      />
     </div>
     <div class="px-5 pb-5 pt-9 relative">
-    
-    <NuxtLink :to="`/product/${props.product.id}`" ><h3 class="text-lg font-
-    bold mb-4 text-limit limit-2">{{ props.product.name }}</h3></NuxtLink>
-    
-    <div class="flex justify-between items-center">
-    <span class="text-sm font-normal">{{ props.product.category }}</span>
-    <span class="text-sm font-normal">${{ props.product.price }}</span>
+      <NuxtLink :to="`/product/${props.product.id}`"
+        ><h3 class="text-lg font-bold mb-4 text-limit limit-2">
+          {{ props.product.name }}
+        </h3></NuxtLink
+      >
+
+      <div class="flex justify-between items-center">
+        <span class="text-sm font-normal">{{ props.product.category }}</span>
+        <span class="text-sm font-normal">${{ props.product.price }}</span>
+      </div>
+      <br>
+   
+      <div>
+        <span class="text-sm font-normal">{{ props.product.description }}</span>
+      </div>
+      
+      <!-- Shopping Cart Button -->
+      <div
+        :class="`cursor-pointer absolute -top-5 right-7 w-[50px] h-[50px]
+        ${props.product.isCart ? 'bg-blue-600 text-white' : 'bg-white'} shadow-xl
+        rounded-full flex justify-center items-center hover:bg-blue-600 hover:text-white
+        transition duration-300`"
+      >
+        <i class="ri-shopping-cart-2-line"></i>
+      </div>
+
+      <!-- Delete Button -->
+      <div
+        @click="deleteProduct"
+        :class="`cursor-pointer absolute -top-5  w-[50px] h-[50px]
+          bg-white text-gray-500 shadow-xl rounded-full flex justify-center items-center
+          hover:bg-red-600 hover:text-white transition duration-300 ${isDeleting ? 'opacity-50' : ''}`"
+        v-if="!isDeleting"
+      >
+        <i class="ri-delete-bin-7-fill"></i>
+      </div>
+
+      <!-- Show a loading spinner while deleting -->
+      <div v-if="isDeleting" class="absolute -top-5 right-7 w-[50px] h-[50px] flex justify-center items-center">
+        <i class="ri-loader-line animate-spin text-gray-500"></i>
+      </div>
     </div>
-    <div :class="`cursor-pointer absolute -top-5 right-7 w-[50px] h-[50px]
-    ${props.product.isCart ? 'bg-blue-600 text-white' : 'bg-white'} shadow-xl
-    rounded-full flex justify-center items-center hover:bg-blue-600 hover:text-white
-    transition duration-300`">
-    <i class="ri-shopping-cart-2-line"></i>
-    </div>
-    </div>
-    </section>
-    </template>
+  </section>
+</template>
--- ecommerce/pages/index.vue
+++ ecommerce/pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>Ini Halaman Index / Home</h1>
-</template>
+    <div class="grid h-screen place-content-center gap-3">
+        <h1 class="text-5xl text-blue-500">Selamat datang di E-commerce Kami</h1>
+        <div class=" flex items-center justify-center">
+        <img src="../public/images/icons/images.png" alt="Welcome Image">
+        </div>
+      <p class="text-xl text-red-600 text-center">
+        Kalian Bisa Membeli Dan Menjual Produk Di Sini
+      </p>
+      <NuxtLink href="/login" class="mx-auto rounded-xl bg-orange-700 px-20 py-4 text-white">
+        Selengkapnya
+      </NuxtLink>
+    </div>
+  </template>
+  
